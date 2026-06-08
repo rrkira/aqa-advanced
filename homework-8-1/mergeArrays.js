015721ee468020fc6b7272505f152d2b3a30ef25
Kira <qakiira@example.com~>
2026-06-08 13:08:01 +0300

    Homework 8.1 arrays

---
+++ homework-8-1/mergeArrays.js
@@ -0,0 +1,9 @@
+// два масиви чисел
+const firstArray = [1, 2, 3];
+const secondArray = [4, 5, 6];
+
+// об'єднуємо через оператор spread
+const mergedArray = [...firstArray, ...secondArray];
+
+// вивід
+console.log(mergedArray);
